f76f1d2330c2d4c606806e2280f5d7c3567b15e1
Yasharth <weirdmailid@gmail.com>
2020-12-16 00:00:15 +0530

    Not growing the list

--- day_15.py
+++ day_15.py
@@ -1,4 +1,7 @@
 import collections
+import time
+
+start = time.time()
 
 puzzle_input = [0,13,16,17,1,10,6]
 game_setup = collections.OrderedDict({k: [v + 1] for k,v in zip(puzzle_input, range(len(puzzle_input)))})
@@ -6,11 +9,11 @@ game_setup = collections.OrderedDict({k: [v + 1] for k,v in zip(puzzle_input, ra
 turn = len(game_setup.keys()) + 2
 last_spoken = 0
 
-
 for t in range(turn, 30000001):
     if last_spoken in game_setup:
-        game_setup[last_spoken].append(t - 1)
-        last_spoken = game_setup[last_spoken][-1] - game_setup[last_spoken][-2]
+        temp = (t - 1) - game_setup[last_spoken][0]
+        game_setup[last_spoken][0] = t - 1
+        last_spoken = temp
     
     elif last_spoken not in game_setup:
         game_setup[last_spoken] = [t - 1]
