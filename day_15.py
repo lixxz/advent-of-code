8a1e095cf3e685fbcfefe7e079349c187cfead99
Yasharth <weirdmailid@gmail.com>
2020-12-15 18:29:32 +0530

    OrderedDict

--- day_15.py
+++ day_15.py
@@ -1,7 +1,7 @@
-# Python 3.7+
-# Takes ~26s for the bigger input
+import collections
+
 puzzle_input = [0,13,16,17,1,10,6]
-game_setup = {k: [v + 1] for k,v in zip(puzzle_input, range(len(puzzle_input)))}
+game_setup = collections.OrderedDict({k: [v + 1] for k,v in zip(puzzle_input, range(len(puzzle_input)))})
 
 turn = len(game_setup.keys()) + 2
 last_spoken = 0
